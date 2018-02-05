26fb775d6a65b1c39011d425d11f2ae9d57dddd6
AdamBrMSFT <adambr@microsoft.com>
2018-02-05 15:28:42 -0800

    Adding files

---
+++ MyUWPGraphApp01/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Graph;
+
+
+//<Application.Resources>
+//    <x:String x:Key="ida:ClientId">935c61af-136a-4671-b4f6-cabf7964bfb4</x:String>
+//    <x:String x:Key="ida:AADInstance">https://login.microsoftonline.com/</x:String>
+//    <x:String x:Key="ida:Domain">microsoft.onmicrosoft.com</x:String>
+//</Application.Resources>
+
+namespace MyUWPGraphApp01
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        static GraphServiceClient MSGraph = null;
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MSGraph = MicrosoftGraphContext.GetAuthenticatedClient(App.Current.Resources["ida:ClientId"].ToString());
+
+            var user = await MSGraph.Me.Request().GetAsync();
+            TextBlock1.Text = String.Format("{0} - '{1}'", user.DisplayName, user.Id);
+
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var mgr = await MSGraph.Me.Manager.Request().GetAsync();
+
+            TextBlock1.Text = mgr.Id;
+                        
+        }
+    }
+}
---
+++ MyUWPGraphApp01/MicrosoftGraphContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Windows.Security.Authentication.Web.Core;
+using Windows.Security.Credentials;
+
+namespace MyUWPGraphApp01
+{
+    public class MicrosoftGraphContext
+    {
+        private static string _clientId = null;
+        private static string _scopes = null;
+        public static GraphServiceClient GetAuthenticatedClient(string clientId, string scopes = "")
+        {
+            _clientId = clientId;
+            _scopes = scopes;
+            var graphClient = new GraphServiceClient(
+                                                new DelegateAuthenticationProvider(
+                                                    async (requestMessage) =>
+                                                    {
+                                                        var token = await GetTokenForUserAsync();
+                                                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
+                                                    }));
+            return graphClient;
+        }
+        public static async Task<string> GetTokenForUserAsync()
+        {
+            //for most Enteprise apps, we only care about AAD version of MSGraph
+            string authority = "organizations";
+            // string resource = "https://graph.windows.net";   // This is the Azure AD Graph not MS Graph...
+            string resource = "https://graph.microsoft.com";    //Microsoft Graph
+            string TokenForUser = null;
+
+            var wap = await WebAuthenticationCoreManager.FindAccountProviderAsync("https://login.microsoft.com", authority);
+
+            // craft the token request for the Graph api
+            //What is the correct scope?
+            WebTokenRequest wtr = new WebTokenRequest(wap, _scopes, _clientId);
+            wtr.Properties.Add("resource", resource);
+
+            WebTokenRequestResult wtrr = await WebAuthenticationCoreManager.RequestTokenAsync(wtr);
+            if (wtrr.ResponseStatus == WebTokenRequestStatus.Success)
+            {
+                TokenForUser = wtrr.ResponseData[0].Token;
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine(wtrr.ResponseError);
+            }
+            return TokenForUser;
+        }
+    }
+}
